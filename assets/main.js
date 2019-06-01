d67ddd01bf4d82c618a6a0f359c83e7125c2d6bb
Michael's <mjlikre@gmail.com>
2019-06-01 01:32:18 -0700

    created the object for the questions and the answer options

---
+++ assets/main.js
@@ -0,0 +1,37 @@
+var randQuestions = {
+    question1: {
+        queston: "What does John Wick call his dog?",
+        answer: ["Good dog", 'Good boy', 'Prince of Barkness', 'Chew Barka']
+    },
+    question2: {
+        question: "Who said this line 'You either die a hero or live long enough to see yourself becoem the villian.'?",
+        answer: ['The Joker', 'Chewbacca', 'Two face', 'Bane' ]
+    },
+    question3: {
+        quesiton: "How did Red Skull become the guardian of the soul stone?",
+        answer: ["He volunteered himself", "He tried to hold the space stone", "Captain America sent him there with a photon blast", "He was never the guardian" ]
+    },
+    question4: {
+        qustion: "What did Tony Stark wanted to eat when he got off the plane in Iron Man 1?",
+        answe: ["Doritos", "Shawarma", "Chesse cake", 'Cheese burger']
+
+    },
+    question5: {
+        question: "Who is Heisenberg?",
+        answer: ['TimTim', 'Walt Wiltman', "Walter White", "Walter Green"]
+
+    },
+    question6: {
+        question: "Who plays Ron Woodroof in Dallas Buyer's Club?",
+        answer: ['Matthew Roberts', "Matthew McConaughey", "Jared Letto", "Leonardo DiCaprio"]
+    },
+    question7: {
+        question: "In what movie did Leonardo DiCaprio win his first Oscars?",
+        answer: ['The Revenant', 'Titanic', 'Wolf Of Walstreet', 'The Great Gatzby']
+    },
+    question8: {
+        question: "What was Thor's code to access the Queenjet?",
+        answer: ['God of Thunder', 'Strongest Avenger', 'Ruler of Asgard', 'Pointbreak']
+    }
+}
+
