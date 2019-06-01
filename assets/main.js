02cf201996674e4ed1dacd8c92b84eb7be816c23
Michael's <mjlikre@gmail.com>
2019-06-01 16:44:17 -0700

    html file done, js logic kind of done, there might be a problem with the question object selection

--- assets/main.js
+++ assets/main.js
@@ -13,7 +13,7 @@ var randQuestions = {
     },
     question4: {
         qustion: "What did Tony Stark wanted to eat when he got off the plane in Iron Man 1?",
-        answe: ["Doritos", "Shawarma", "Chesse cake", 'Cheese burger']
+        answer: ["Doritos", "Shawarma", "Chesse cake", 'Cheese burger']
 
     },
     question5: {
@@ -30,8 +30,32 @@ var randQuestions = {
         answer: ['The Revenant', 'Titanic', 'Wolf Of Walstreet', 'The Great Gatzby']
     },
     question8: {
-        question: "What was Thor's code to access the Queenjet?",
+        question: "What was Thor's access code to the Queenjet?",
         answer: ['God of Thunder', 'Strongest Avenger', 'Ruler of Asgard', 'Pointbreak']
     }
 }
+
+var questionChosen = false;
+var questionAnswered = false; 
+var obj;
+
+function start() {
+    for (var key in randQuestions){
+        obj = randQuestions[key];
+        $('#question').text(obj.question);
+        $('#option1').text(obj.answer[0]);
+        $('#option2').text(obj.answer[1]);
+        $('#option3').text(obj.answer[2]);
+        $('#option4').text(obj.answer[3]);
+    }
+}
+
+start()
+// var time = 30;
+// var invervalId;
+
+
+// function timer(){
+//     intervalId = setInterval(count, 1000)
+// }
 
